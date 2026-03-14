8ce839ff6913d7e64f862190a97f79706496814b
Ignacio Gareca <newignaciogareca@gmail.com>
2026-03-14 18:54:17 -0300

    cambio de reglas eslint 5

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,7 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript", "next/recommended"),
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
 export default eslintConfig;
